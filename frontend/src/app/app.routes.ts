43fd0c326ceb6a886fb61ecbc50a356de91e23d6
Ayan Gorain <100009409+ayan-gorain@users.noreply.github.com>
2025-09-19 15:31:14 +0530

    kjd

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
-    // {path:'',redirectTo:'/signup',pathMatch:'full'},
-    // {path:'signup',component:SignupComponent}
+    {path:'',redirectTo:'/signup',pathMatch:'full'},
+    {path:'signup',component:SignupComponent}
 ];
